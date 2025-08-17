458c4bb3de6e9a73530d8c8ec966293d5fdddc2e
pranav.patil <pranav.patil@zeusnumerix.com>
2025-08-17 14:44:07 +0530

    dev: created express server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/// 
+// console.log("Starting a new Project!");
+const express = require("express");
+
+const app = express();
+
+app.use( "/", (req, res) => {
+    res.send( "Hello from the / server!");
+});
+app.use(( req, res) => {
+    res.send( "Hello from the server!");
+});
+
+
+
+app.listen(3000, ()=> {
+    console.log( "Server is listening on port 3000...");
+});
