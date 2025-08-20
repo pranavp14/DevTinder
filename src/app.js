0ffc37f7f827ce267bf9369b8a50b9dc554458a5
pranav.patil <pranav.patil@zeusnumerix.com>
2025-08-21 01:21:56 +0530

    dev: added error handling in routing.

--- src/app.js
+++ src/app.js
@@ -9,15 +9,35 @@ const { adminAuth, userAuth } = require("./middleware/auth");
 app.use("/admin", adminAuth);
 
 app.get("/user/login", (req, res) => {
+    
     res.send("Welcome to the User Login Page");     
 });
 
 app.get("/admin/dashboard", (req, res) => {
+    try {
+        // Simulating some admin dashboard logic        
     res.send("Welcome to the Admin Dashboard");
+    } catch (error) {
+        res.status(500).send("Internal Server Error");          
+    }
 }); 
 
 app.use("/user/data", userAuth, (req, res) => {
     res.send("get user data");
+});
+
+app.use("/", (err, req, res, next ) => {
+    if (err) {
+        console.error("Error occurred:", err);          
+        // res.status(500).send("Internal Server Error");
+    } else {  
+        next();
+        // res.send("Welcome to the Home Page");
+    }   
+});
+
+app.use("/yes", ( req, res ) => {
+    res.send("Yes, this is a response from the /yes route");
 });
 
 app.server = app.listen(3000, () => {
